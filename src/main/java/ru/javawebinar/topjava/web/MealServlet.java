52d6ddc65c7c0233fe555538d77ac1956a8dc157
chumakig86 <chumak_ig@yahoo.com>
2017-11-07 12:31:00 +0200

    MealServlet and meals.jsp implementation (display meal list)

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,22 +1,42 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
+import static ru.javawebinar.topjava.util.MealsUtil.getFilteredWithExceeded;
+
+/**
+ * Created by vlad on 27-Jul-17.
+ */
 
 public class MealServlet extends HttpServlet {
-    private static final Logger log = getLogger(MealServlet.class);
+
+
+    private static final Logger LOG = getLogger(MealServlet.class);
+
+
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("redirect to meals");
-
-
-        response.sendRedirect("meals.jsp");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        LOG.debug("redirect to meals");
+        List<MealWithExceed> meals = getFilteredWithExceeded(
+                MealsUtil.meals,
+                LocalTime.MIN,
+                LocalTime.MAX,
+                2000);
+        req.setAttribute("meals", meals);
+        req.getRequestDispatcher("/meals.jsp").forward(req, resp);
+//        resp.sendRedirect("meals.jsp");
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,17 +1,53 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: user
-  Date: 06.11.2017
-  Time: 21:10
-  To change this template use File | Settings | File Templates.
---%>
+<%@ page import="ru.javawebinar.topjava.util.TimeUtil" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 <html>
 <head>
     <title>Meals</title>
+    <style>
+
+        .normal {
+            color: green;
+        }
+        .exceeded {
+            color: red;
+        }
+    </style>
 </head>
+
+
 <body>
+
 <h3><a href="index.html">Home</a></h3>
-<h2>Meals</h2>
+<h2>meals List</h2>
+<div>
+    <table border="1" cellpadding="8" cellspacing="0">
+        <thead>
+        <tr>
+            <th>time</th>
+            <th>Description</th>
+            <th>Calories</th>
+        </tr>
+        </thead>
+        <tbody>
+        <c:forEach items="${meals}" var="meal">
+            <jsp:useBean id="meal" scope="page" type="ru.javawebinar.topjava.model.MealWithExceed"/>
+            <tr class="${meal.exceed ? "exceeded" : "normal"}">
+                <td>${meal.dateTime.toLocalDate()} ${meal.dateTime.toLocalTime()}</td>
+
+                <td>${meal.description}</td>
+                <td>${meal.calories}</td>
+            </tr>
+        </c:forEach>
+        </tbody>
+
+
+
+    </table>
+</div>
+
+
 </body>
 </html>
